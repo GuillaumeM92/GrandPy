9a731c35259dae8ae86d9895c72ee57f63fa9ba0
GuillaumeM92 <gemacx@gmail.com>
2020-06-15 21:18:48 +0200

    modified gmaps api to search in france first

--- utils/googlemaps.py
+++ utils/googlemaps.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_json_response(parsed_question):
-    gmaps_json_response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + parsed_question + "&key=AIzaSyADzdSV9PVtSJ4QtuK3EgKQWF55egnZR0w&language=fr&callback=initMap").json()
+    gmaps_json_response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + parsed_question + "&key=AIzaSyADzdSV9PVtSJ4QtuK3EgKQWF55egnZR0w&language=fr&region=FR&callback=initMap").json()
     return gmaps_json_response
 
 def get_formatted_address(gmaps_json_response):
