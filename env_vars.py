f0663934afbd9bb9e52ba57fa2b5a77437d3da18
GuillaumeM92 <gemacx@gmail.com>
2020-06-18 11:07:06 +0200

    added the api key for the website

--- env_vars.py
+++ env_vars.py
@@ -1,4 +1,5 @@
 import os
 
 # Replace "os.environ.get('GOOGLE_API_KEY')" with your own google maps API key
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+GOOGLE_API_KEY = "AIzaSyADzdSV9PVtSJ4QtuK3EgKQWF55egnZR0w"
+# GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
